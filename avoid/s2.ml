663a79a8a79612c8691554246a9bb7ab7ad1f828
Hikaru Suido <hikaru.suido@gmail.com>
2017-06-09 14:08:04 +0900

    Remove unnecessary files again

--- avoid/s2.ml
+++
@@ -1,9 +0,0 @@
-let rec stairs2 (r) =
-        if r<0 then
-                failwith "Error"
-        else
-                match r with
-                | 1 -> 1
-                | 2 -> 2
-                | _ -> stairs2 (r-2) + stairs2 (r-1)
-;;
